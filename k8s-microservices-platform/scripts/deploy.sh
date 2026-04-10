a64dd92415b168c76c50e064c2042d23a72d44a7
Bhavyashree.R <Bhavyashree2201036@gmail.com>
2026-04-10 17:10:24 +0530

    Add deployment script for microservices with Helm

--- k8s-microservices-platform/scripts/deploy.sh
+++ k8s-microservices-platform/scripts/deploy.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+kubectl create namespace microservices
+
+helm install postgres ./helm/postgres -n microservices
+helm install order ./helm/order-service -n microservices
+helm install api ./helm/api-gateway -n microservices
+helm install frontend ./helm/frontend -n microservices
+
+kubectl apply -f k8s/ingress.yaml
