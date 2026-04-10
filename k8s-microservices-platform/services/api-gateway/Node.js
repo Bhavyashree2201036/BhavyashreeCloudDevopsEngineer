0cddc27081b63ec2da726fba072e2dad76ced08d
Bhavyashree.R <Bhavyashree2201036@gmail.com>
2026-04-10 16:25:19 +0530

    Implement API Gateway with orders endpoint

--- k8s-microservices-platform/services/api-gateway/Node.js
+++ k8s-microservices-platform/services/api-gateway/Node.js
@@ -1,1 +1,17 @@
 
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+app.get('/orders', async (req, res) => {
+    try {
+        const response = await axios.get('http://order-service:3000/orders');
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).send("Service unavailable");
+    }
+});
+
+app.listen(3000, () => console.log("API Gateway running"));
